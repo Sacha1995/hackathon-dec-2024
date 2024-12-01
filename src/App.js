2c9b4c8607df2f37bd1b20130ca81586235b166f
Sacha Uijlen <sacha-u@hotmail.com>
2024-12-01 12:12:02 +0000

    first version of spinner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Spinner from "./components/Spinner";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
+      <Title text="The Best Christmas Game EVER!" />
       <Spinner />
     </div>
   );
