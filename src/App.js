ba3b2d22638077821a13923fca8275dc50e712cb
Sacha Uijlen <sacha-u@hotmail.com>
2024-12-01 12:26:35 +0000

    round two

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import Button from "./components/Button";
 import Spinner from "./components/Spinner";
 import Title from "./components/Title";
+import { useState } from "react";
 
 function App() {
+  const [round, setRound] = useState(1);
+
   return (
     <div className="App">
       <Title text="The Best Christmas Game EVER!" />
-      <Spinner />
+      <Spinner round={round} />
+      <div className="btns">
+        {round === 1 && (
+          <Button
+            text="All the presents have been taken"
+            onClick={() => {
+              setRound(2);
+            }}
+          />
+        )}
+        {round === 2 && (
+          <Button
+            text="Start over"
+            onClick={() => {
+              setRound(1);
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 }
